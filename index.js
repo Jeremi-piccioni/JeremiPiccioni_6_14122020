599c331a437910ab65e69dd56206cf9e2fb74cab
Jeremi <jeremi.piccioni@gmail.com>
2020-12-19 11:49:32 +0100

    1st route working

--- index.js
+++ index.js
@@ -1,32 +1,46 @@
 const express =require('express')
 const app = express()
 const dotenv = require('dotenv')
+
 const mongoose = require('mongoose')
 
 // Import Routes
 const authRoute = require('./routes/auth')
+const User = require('./model/User')
 
 dotenv.config()
 
 
 // mongodb+srv://AdminMongoDB:<password>@cluster0.m433z.mongodb.net/<dbname>?retryWrites=true&w=majority // connection Password string
 // DB_CONNECT = mongodb+srv://AdminMongoDB:<password>@cluster0.m433z.mongodb.net/<dbname>?retryWrites=true&w=majority
+// cle pour connection a la BD local : 'mongodb://localhost:27017/pecockoLocal'
+    
 
 //contect to Mongo DB
+console.log('CloudMongoDBSting='+ process.env.DB_CONNECT)
+  
+//mongoose.connect('mongodb+srv://AdminMongoDB:AdminMongoDB2018@cluster0.m433z.mongodb.net/Cluster0?retryWrites=true&w=majority',
 
-mongoose.connect("'" + process.env.DB_CONNECT + "'" ,
+mongoose.connect(process.env.DB_CONNECT,
     { useNewUrlParser: true },
     //{ useUnifiedTopology: true },  // Ou faut il coller cette objet pour ne plus avoir le warning ?
-    ()=> {console.log('Connected to MongoDB BABY !!')}) 
+   
+    (err)=> {console.log('erreur de connection à la DB : '+ err)
+    User.create({
+        "email": "totooihsafoihfda@sds.com",
+        "password": "1223345656"
+})}) 
 
 // Middleware
 app.use(express.json())
 
+// Setting up listening port
+app.listen(3000, () => console.log('Back End server running smoothly'))
+
 //Route Middleware
 app.use('/api/user', authRoute)     // route à changer par : /api/auth
 
-// Setting up listening port
-app.listen(3000, () => console.log('Back End server running smoothly'))
+
 
 
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,14 @@
 const router = require('express').Router()  //Router ??
 const User = require('../model/User')
 
+/* DB_CONNECT = mongodb+srv://AdminMongoDB:AdminMongoDB2018@cluster0.m433z.mongodb.net/<dbname>?retryWrites=true&w=majority */
 
-router.post('/register', /*async*/(req, res) => {    //  /register à changer en /signup   <-- register route a changer
+
+// router.post('/register', (req,res) =>{  // Simple request Tester on route /register <-- cette requete fomctionne nickel
+//     res.send('it should work')
+// })
+
+router.post('/register', async(req, res) => {    //  /register à changer en /signup   <-- register route a changer
    
     const user = new User({    /*req.body */
        email: req.body.email,
@@ -10,20 +16,20 @@ router.post('/register', /*async*/(req, res) => {    //  /register à changer en
       })
       try {
           console.log('passe dans le try')
-            const savedUser = /*await*/ user.save().then( (savedUser) => res.send(savedUser) )
+            const savedUser = await user.save() //.then( (savedUser) => res.send(savedUser) )
          // console.log('passe dans le then')
-                                                   .catch( (err) => res.status(400).send(err) ) 
+                                                  // .catch( (err) => res.status(400).send(err) ) 
           console.log('passe le catch apres le then')  
+          res.send(savedUser)
               
         }  catch (err) {
-         res.status(400).send(err) 
+         console.log(err)
+         //res.status(400).send(err) 
         }                                  // type de reponse attendu : { message: string } ??
     })            
     
     
-router.post('/register', (req,res) =>{  // Simple request Tester on route /register <-- cette requete fomctionne nickel
-        res.send('it should work')
-    })
+
                
 module.exports = router
 
